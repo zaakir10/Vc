0c922d4579ad79263b2f6d6afa5dd3150792a97c
MrAbhiX <96232318+MrAbhiX@users.noreply.github.com>
2022-08-02 12:45:22 +0530

    Update __init__.py

--- Client/__init__.py
+++ Client/__init__.py
@@ -1,1 +1,8 @@
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+from Config import SESSION, API_ID, API_HASH
 
+
+
+app = Client(SESSION, api_id=API_ID, api_hash=API_HASH)
+pytgcalls = PyTgCalls(app)
