f11110ba415d631cf05d2f62e22def6ec73331e3
MrAbhiX <96232318+MrAbhiX@users.noreply.github.com>
2022-08-02 20:37:55 +0530

    Create Admins.py

---
+++ Plugins/Admins.py
@@ -0,0 +1,131 @@
+
+@app.on_message(
+    filters.command(["skip", "next"], config.PREFIXES)
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def skip_track(_, message: Message, lang):
+    chat_id = message.chat.id
+    group = get_group(chat_id)
+    if group["loop"]:
+        await skip_stream(group["now_playing"], lang)
+    else:
+        queue = get_queue(chat_id)
+        if len(queue) > 0:
+            next_song = await queue.get()
+            if not next_song.parsed:
+                ok, status = await next_song.parse()
+                if not ok:
+                    raise Exception(status)
+            set_group(chat_id, now_playing=next_song)
+            await skip_stream(next_song, lang)
+            await delete_messages([message])
+        else:
+            set_group(chat_id, is_playing=False, now_playing=None)
+            await set_title(message, "")
+            try:
+                await pytgcalls.leave_group_call(chat_id)
+                k = await message.reply_text(lang["queueEmpty"])
+            except (NoActiveGroupCall, GroupCallNotFound):
+                k = await message.reply_text(lang["notActive"])
+            await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["m", "mute"], config.PREFIXES) & ~filters.private & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def mute_vc(_, message: Message, lang):
+    chat_id = message.chat.id
+    try:
+        await pytgcalls.mute_stream(chat_id)
+        k = await message.reply_text(lang["muted"])
+    except (NoActiveGroupCall, GroupCallNotFound):
+        k = await message.reply_text(lang["notActive"])
+    await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["um", "unmute"], config.PREFIXES)
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def unmute_vc(_, message: Message, lang):
+    chat_id = message.chat.id
+    try:
+        await pytgcalls.unmute_stream(chat_id)
+        k = await message.reply_text(lang["unmuted"])
+    except (NoActiveGroupCall, GroupCallNotFound):
+        k = await message.reply_text(lang["notActive"])
+    await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["ps", "pause"], config.PREFIXES)
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def pause_vc(_, message: Message, lang):
+    chat_id = message.chat.id
+    try:
+        await pytgcalls.pause_stream(chat_id)
+        k = await message.reply_text(lang["paused"])
+    except (NoActiveGroupCall, GroupCallNotFound):
+        k = await message.reply_text(lang["notActive"])
+    await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["rs", "resume"], config.PREFIXES)
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def resume_vc(_, message: Message, lang):
+    chat_id = message.chat.id
+    try:
+        await pytgcalls.resume_stream(chat_id)
+        k = await message.reply_text(lang["resumed"])
+    except (NoActiveGroupCall, GroupCallNotFound):
+        k = await message.reply_text(lang["notActive"])
+    await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["stop", "leave"], config.PREFIXES)
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def leave_vc(_, message: Message, lang):
+    chat_id = message.chat.id
+    set_group(chat_id, is_playing=False, now_playing=None)
+    await set_title(message, "")
+    clear_queue(chat_id)
+    try:
+        await pytgcalls.leave_group_call(chat_id)
+        k = await message.reply_text(lang["leaveVC"])
+    except (NoActiveGroupCall, GroupCallNotFound):
+        k = await message.reply_text(lang["notActive"])
+    await delete_messages([message, k])
