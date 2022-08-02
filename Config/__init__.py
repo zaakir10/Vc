949f3a42fa52c0a2c2a1537a3233278a958b2e42
MrAbhiX <96232318+MrAbhiX@users.noreply.github.com>
2022-08-02 20:27:39 +0530

    Update __init__.py

--- Config/__init__.py
+++ Config/__init__.py
@@ -1,1 +1,9 @@
 from Config.Config import config
+
+SESSION = config.SESSION
+API_ID = config.API_ID
+API_HASH = config.API_HASH
+SUDOERS = config.SUDOERS
+QUALITY = config.QUALITY
+LANGUAGE = config.LANGUAGE
+PREFIX = config.PREFIX
