ffd3073221082875842d67434852aa265c8d3b4e
MrAbhiX <96232318+MrAbhiX@users.noreply.github.com>
2022-08-02 19:11:26 +0530

    Update __init__.py

--- Core/__init__.py
+++ Core/__init__.py
@@ -2,7 +2,7 @@ from Core.Song import Song
 from Core.Groups import (
     get_group, get_queue, set_group, set_title, all_groups, clear_queue,
     set_default, shuffle_queue)
-from Core.Function import (
+from Core.Functions import (
     ydl, xd, search, restart, skip_stream, check_yt_url,
     extract_args, start_stream, generate_cover, delete_messages,
     get_youtube_playlist)
