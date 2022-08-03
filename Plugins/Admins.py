5e8b6cd75598c998e7bf2ff25350cba3c1251feb
MrAbhiX <96232318+MrAbhiX@users.noreply.github.com>
2022-08-03 15:45:22 +0530

    Update Admins.py

--- Plugins/Admins.py
+++ Plugins/Admins.py
@@ -148,3 +148,348 @@ async def leave_vc(_, message: Message, lang):
     except (NoActiveGroupCall, GroupCallNotFound):
         k = await message.reply_text(lang["notActive"])
     await delete_messages([message, k])
+
+ @app.on_message(
+    filters.command(["list", "queue"])
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@handle_error
+async def queue_list(_, message: Message, lang):
+    chat_id = message.chat.id
+    queue = get_queue(chat_id)
+    if len(queue) > 0:
+        k = await message.reply_text(str(queue), disable_web_page_preview=True)
+    else:
+        k = await message.reply_text(lang["queueEmpty"])
+    await delete_messages([message, k])
+
+@app.on_message(
+    filters.command(["mix", "shuffle"])
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def shuffle_list(_, message: Message, lang):
+    chat_id = message.chat.id
+    if len(get_queue(chat_id)) > 0:
+        shuffled = shuffle_queue(chat_id)
+        k = await message.reply_text(str(shuffled), disable_web_page_preview=True)
+    else:
+        k = await message.reply_text(lang["queueEmpty"])
+    await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["loop", "repeat"])
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def loop_stream(_, message: Message, lang):
+    chat_id = message.chat.id
+    group = get_group(chat_id)
+    if group["loop"]:
+        set_group(chat_id, loop=False)
+        k = await message.reply_text(lang["loopOff"])
+    elif group["loop"] == False:
+        set_group(chat_id, loop=True)
+        k = await message.reply_text(lang["loopOn"])
+    await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["mode", "switch"])
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def switch_mode(_, message: Message, lang):
+    chat_id = message.chat.id
+    group = get_group(chat_id)
+    if group["is_video"]:
+        set_group(chat_id, is_video=False)
+        k = await message.reply_text(lang["audioMode"])
+    else:
+        set_group(chat_id, is_video=True)
+        k = await message.reply_text(lang["videoMode"])
+    await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["lang", "language"])
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def set_lang(_, message: Message, lang):
+    chat_id = message.chat.id
+    lng = extract_args(message.text)
+    if lng != "":
+        langs = [
+            file.replace(".json", "")
+            for file in os.listdir(f"{os.getcwd()}/lang/")
+            if file.endswith(".json")
+        ]
+        if lng == "list":
+            k = await message.reply_text("\n".join(langs))
+        elif lng in langs:
+            set_group(chat_id, lang=lng)
+            k = await message.reply_text(lang["langSet"] % lng)
+        else:
+            k = await message.reply_text(lang["notFound"])
+        await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["ep", "export"])
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def export_queue(_, message: Message, lang):
+    chat_id = message.chat.id
+    queue = get_queue(chat_id)
+    if len(queue) > 0:
+        data = json.dumps([song.to_dict() for song in queue], indent=2)
+        filename = f"{message.chat.username or message.chat.id}.json"
+        with open(filename, "w") as file:
+            file.write(data)
+        await message.reply_document(
+            filename, caption=lang["queueExported"] % len(queue)
+        )
+        os.remove(filename)
+        await delete_messages([message])
+    else:
+        k = await message.reply_text(lang["queueEmpty"])
+        await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["ip", "import"])
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def import_queue(_, message: Message, lang):
+    if not message.reply_to_message or not message.reply_to_message.document:
+        k = await message.reply_text(lang["replyToAFile"])
+        return await delete_messages([message, k])
+    chat_id = message.chat.id
+    filename = await message.reply_to_message.download()
+    data_str = None
+    with open(filename, "r") as file:
+        data_str = file.read()
+    try:
+        data = json.loads(data_str)
+    except json.JSONDecodeError:
+        k = await message.reply_text(lang["invalidFile"])
+        return await delete_messages([message, k])
+    try:
+        temp_queue = []
+        for song_dict in data:
+            song = Song(song_dict["yt_url"], message)
+            song.title = song_dict["title"]
+            temp_queue.append(song)
+    except BaseException:
+        k = await message.reply_text(lang["invalidFile"])
+        return await delete_messages([message, k])
+    group = get_group(chat_id)
+    queue = get_queue(chat_id)
+    if group["is_playing"]:
+        for _song in temp_queue:
+            await queue.put(_song)
+    else:
+        song = temp_queue[0]
+        set_group(chat_id, is_playing=True, now_playing=song)
+        ok, status = await song.parse()
+        if not ok:
+            raise Exception(status)
+        try:
+            await start_stream(song, lang)
+        except (NoActiveGroupCall, GroupCallNotFound):
+            peer = await app.resolve_peer(chat_id)
+            await app.send(
+                CreateGroupCall(
+                    peer=InputPeerChannel(
+                        channel_id=peer.channel_id,
+                        access_hash=peer.access_hash,
+                    ),
+                    random_id=app.rnd_id() // 9000000000,
+                )
+            )
+            await start_stream(song, lang)
+        for _song in temp_queue[1:]:
+            await queue.put(_song)
+    k = await message.reply_text(lang["queueImported"] % len(temp_queue))
+    await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["pl", "playlist"])
+    & ~filters.private
+    & ~filters.edited
+)
+@register
+@language
+@only_admins
+@handle_error
+async def import_playlist(_, message: Message, lang):
+    chat_id = message.chat.id
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+    else:
+        text = extract_args(message.text)
+    if text == "":
+        k = await message.reply_text(lang["notFound"])
+        return await delete_messages([message, k])
+    if "youtube.com/playlist?list=" not in text:
+        k = await message.reply_text(lang["invalidFile"])
+        return await delete_messages([message, k])
+    try:
+        temp_queue = get_youtube_playlist(text, message)
+    except BaseException:
+        k = await message.reply_text(lang["notFound"])
+        return await delete_messages([message, k])
+    group = get_group(chat_id)
+    queue = get_queue(chat_id)
+    if not group["is_playing"]:
+        song = await temp_queue.__anext__()
+        set_group(chat_id, is_playing=True, now_playing=song)
+        ok, status = await song.parse()
+        if not ok:
+            raise Exception(status)
+        try:
+            await start_stream(song, lang)
+        except (NoActiveGroupCall, GroupCallNotFound):
+            peer = await app.resolve_peer(chat_id)
+            await app.send(
+                CreateGroupCall(
+                    peer=InputPeerChannel(
+                        channel_id=peer.channel_id,
+                        access_hash=peer.access_hash,
+                    ),
+                    random_id=app.rnd_id() // 9000000000,
+                )
+            )
+            await start_stream(song, lang)
+        async for _song in temp_queue:
+            await queue.put(_song)
+        queue.get_nowait()
+    else:
+        async for _song in temp_queue:
+            await queue.put(_song)
+    k = await message.reply_text(lang["queueImported"] % len(group["queue"]))
+    await delete_messages([message, k])
+
+
+@app.on_message(
+    filters.command(["update", "restart"])
+    & ~filters.private
+    & ~filters.edited
+)
+@language
+@only_admins
+@handle_error
+async def update_restart(_, message: Message, lang):
+    chats = all_groups()
+    stats = await message.reply_text(lang["update"])
+    for chat in chats:
+        try:
+            await pytgcalls.leave_group_call(chat)
+        except (NoActiveGroupCall, GroupCallNotFound):
+            pass
+    await stats.edit_text(lang["restart"])
+    Thread(target=restart()).start()
+
+
+@pytgcalls.on_stream_end()
+@language
+@handle_error
+async def stream_end(_, update: Update, lang):
+    if isinstance(update, StreamAudioEnded) or isinstance(update, StreamVideoEnded):
+        chat_id = update.chat_id
+        group = get_group(chat_id)
+        if group["loop"]:
+            await skip_stream(group["now_playing"], lang)
+        else:
+            queue = get_queue(chat_id)
+            if len(queue) > 0:
+                next_song = await queue.get()
+                if not next_song.parsed:
+                    ok, status = await next_song.parse()
+                    if not ok:
+                        raise Exception(status)
+                set_group(chat_id, now_playing=next_song)
+                await skip_stream(next_song, lang)
+            else:
+                if xd.get(chat_id) is not None:
+                    try:
+                        await safone[chat_id].delete()
+                    except BaseException:
+                        pass
+                await set_title(chat_id, "", client=app)
+                set_group(chat_id, is_playing=False, now_playing=None)
+                await pytgcalls.leave_group_call(chat_id)
+
+
+@pytgcalls.on_closed_voice_chat()
+@handle_error
+async def closed_vc(_, chat_id: int):
+    if chat_id not in all_groups():
+        if xd.get(chat_id) is not None:
+            try:
+                await xd[chat_id].delete()
+            except BaseException:
+                pass
+        await set_title(chat_id, "", client=app)
+        set_group(chat_id, now_playing=None, is_playing=False)
+        clear_queue(chat_id)
+
+
+@pytgcalls.on_kicked()
+@handle_error
+async def kicked_vc(_, chat_id: int):
+    if chat_id not in all_groups():
+        if xd.get(chat_id) is not None:
+            try:
+                await xd[chat_id].delete()
+            except BaseException:
+                pass
+        await set_title(chat_id, "", client=app)
+        set_group(chat_id, now_playing=None, is_playing=False)
+        clear_queue(chat_id)
+
+
+@pytgcalls.on_left()
+@handle_error
+async def left_vc(_, chat_id: int):
+    if chat_id not in all_groups():
+        if xd.get(chat_id) is not None:
+            try:
+                await xd[chat_id].delete()
+            except BaseException:
+                pass
+        await set_title(chat_id, "", client=app)
+        set_group(chat_id, now_playing=None, is_playing=False)
+        clear_queue(chat_id)
